1cfd186c29a091ff9bc84ce2e95eb0d3f6273363
Guilherme Maciel Ferreira <guilherme.maciel.ferreira@gmail.com>
2015-11-18 20:54:50 -0200

    Problem #1: test script
    Signed-off-by: Guilherme Maciel Ferreira <guilherme.maciel.ferreira@gmail.com>

---
+++ problem_1/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Success
+./problem_1 helloworld elloworldh
+./problem_1 helloworld lloworldhe
+./problem_1 helloworld loworldhel
+./problem_1 helloworld oworldhell
+./problem_1 helloworld worldhello
+
+# Failure
+./problem_1 helloworld alloworldh
+./problem_1 helloworld llowurldhe
+./problem_1 helloworld loworfdhel
+./problem_1 helloworld owcrldhell
+./problem_1 helloworld worldhillo
