2e58a75ff9c6997381d4ca23fbe46183dde74eef
Guilherme Maciel Ferreira <guilherme.maciel.ferreira@gmail.com>
2015-11-18 20:54:50 -0200

    Problem #2: test script
    Signed-off-by: Guilherme Maciel Ferreira <guilherme.maciel.ferreira@gmail.com>

---
+++ problem_2/test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Pangrams
+./problem_2 "A quick brown fox jumps over the lazy dog"
+./problem_2 "Pack my box with five dozen liquor jugs."
+./problem_2 "Despite having an excellent breakfast, Jacques was amazingly slow."
+
+# Not pangrams
+./problem_2 "This sentence contains A-Z but not the whole alphabet."
+./problem_2 "Guadalajara seems to be a very promising place to work."
+./problem_2 "I can speak bit of german. Ich moechte ein Brezel bitte. Ich mag das"
