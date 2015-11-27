b5705f589133e3300eeb037ebc4c49701a1b38db
Guilherme Maciel Ferreira <guilherme.maciel.ferreira@gmail.com>
2015-11-27 15:27:02 -0200

    Problem #4: prepare presentation rules and records
    Signed-off-by: Guilherme Maciel Ferreira <guilherme.maciel.ferreira@gmail.com>

--- problem_4/test.sh
+++ problem_4/test.sh
@@ -2,4 +2,7 @@
 
 LD_LIBRARY_PATH=. ./problem_4_test
 
-$DEBUGGER ./problem_4 sample/rules1.c3l sample/input1.call
+$DEBUGGER ./problem_4 sample/rules-assignments.c3l sample/calls1.rec
+$DEBUGGER ./problem_4 sample/rules-arithmetic_operations.c3l sample/calls1.rec
+$DEBUGGER ./problem_4 sample/rules-conditional_assignment.c3l sample/calls1.rec
+$DEBUGGER ./problem_4 sample/rules-problem_4.c3l sample/calls1.rec
