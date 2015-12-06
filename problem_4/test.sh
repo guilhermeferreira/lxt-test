bc3b23dfb41f491edf24ced284560f524302cf9a
Guilherme Maciel Ferreira <guilherme.maciel.ferreira@gmail.com>
2015-12-06 02:53:21 -0200

    Problem #4: add valgrind debugger option
    Signed-off-by: Guilherme Maciel Ferreira <guilherme.maciel.ferreira@gmail.com>

--- problem_4/test.sh
+++ problem_4/test.sh
@@ -1,8 +1,32 @@
 #!/bin/sh
 
-LD_LIBRARY_PATH=. ./problem_4_test
+DEBUGGER=""
+if [ "$1" == "DEBUG" ]; then
+	DEBUGGER+="valgrind --tool=memcheck --leak-check=full --track-origins=yes"
+fi
+
+LD_LIBRARY_PATH=. $DEBUGGER ./problem_4_test
+
+echo ""
+echo "---------------------------------------";
+echo ""
 
 $DEBUGGER ./problem_4 sample/rules-assignments.c3l sample/calls1.rec
+
+echo ""
+echo "---------------------------------------";
+echo ""
+
 $DEBUGGER ./problem_4 sample/rules-arithmetic_operations.c3l sample/calls1.rec
+
+echo ""
+echo "---------------------------------------";
+echo ""
+
 $DEBUGGER ./problem_4 sample/rules-conditional_assignment.c3l sample/calls1.rec
+
+echo ""
+echo "---------------------------------------";
+echo ""
+
 $DEBUGGER ./problem_4 sample/rules-problem_4.c3l sample/calls1.rec
